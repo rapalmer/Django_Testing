b4bf72c1b29ebe5bbbaf32b17833b6cb94877605
rapalmer <rapalmer@iastate.edu>
2017-01-19 13:02:12 -0600

    polls app up and running

---
+++ newsite/newsite/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+
+import random
+
+def hello_world(request):
+	return HttpResponse("Hello World")
+
+def root_page(request):
+	return HttpResponse("Root Homepage")
+
+def random_number(request, max_rand=100):
+	random_number = random.randrange(0, int(max_rand))
+	msg = "Random Number Between 0 and %s : %d" % (max_rand, random_number)
+	return HttpResponse(msg)
